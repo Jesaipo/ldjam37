81cb15450e5fd453e351faa7ab6c5eb19234f68a
Jeremy Morel <jeremy.morel.jm@gmail.com>
2016-12-09 22:47:40 +0100

    re-init

---
+++ Assets/Test/parallax&effect/Scripts/parallaxPlan.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class parallaxPlan : MonoBehaviour {
+
+	public List<GameObject> visibleGameObjectTab;
+
+	public float distance;
+
+	public GameObject popLimitation;
+	public GameObject depopLimitation;
+	public float hightSpaceBetweenAsset = 0;
+	public float lowSpaceBetweenAsset = 0;
+
+	public assetGenerator generator;
+
+	private float initSpeed = 0.1f;
+	private bool isInit = false;
+
+	private float actualSpeed = 0.0f;
+
+	private float spaceBetweenAsset = 0.0f;
+	private float speedMultiplicator;
+
+	// Use this for initialization
+	void Start () {
+		if (distance < 0) {
+			speedMultiplicator = 1 - (1 / (1 -distance));
+		} else {
+			speedMultiplicator = 1 - (1 / (1 + distance));
+		}
+		generator.clear ();
+		while (!isInit) {
+			moveAsset (initSpeed);
+			generateAssetIfNeeded ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveAsset (actualSpeed * speedMultiplicator);
+		generateAssetIfNeeded ();
+}
+
+	void moveAsset(float speed){
+		for (int i=0; i<visibleGameObjectTab.Count; i++) {
+			GameObject parrallaxAsset = visibleGameObjectTab[i];
+			Vector3 positionAsset = parrallaxAsset.transform.position;
+			if (!isStillVisible(parrallaxAsset)){
+				parrallaxAsset.SetActive(false);
+				visibleGameObjectTab.Remove(parrallaxAsset);
+				isInit =true;
+			} else {
+				positionAsset.x -= speed;
+				parrallaxAsset.transform.position = positionAsset;
+			}
+		}
+	}
+
+
+	void generateAssetIfNeeded(){
+		if(spaceBetweenLastAndPopLimitation() > spaceBetweenAsset){
+			GameObject asset = generator.generateGameObjectAtPosition(new Vector3(popLimitation.transform.position.x,popLimitation.transform.position.y,this.transform.position.z));
+			asset.transform.parent = this.transform;
+			visibleGameObjectTab.Add(asset);
+
+			generateNewSpaceBetweenAssetValue();
+		}
+	}
+
+
+	void generateNewSpaceBetweenAssetValue(){
+		spaceBetweenAsset = Random.Range (lowSpaceBetweenAsset,hightSpaceBetweenAsset);
+	}
+
+
+	public void setSpeedOfPlan(float newSpeed){
+		if (actualSpeed * newSpeed < 0) {
+			swapPopAndDepop();
+
+			print("Swap");
+		}
+		actualSpeed = newSpeed;
+	}
+
+	void swapPopAndDepop(){
+		GameObject temp = popLimitation;
+		popLimitation = depopLimitation;
+		depopLimitation = temp;
+	}
+
+
+	bool isStillVisible (GameObject parallaxObject) {
+		if (popLimitation.transform.position.x < depopLimitation.transform.position.x) {
+			return (parallaxObject.transform.position.x + (parallaxObject.transform.lossyScale.x / 2) < depopLimitation.transform.position.x);// probl"me ici
+		} else {
+			return (parallaxObject.transform.position.x + (parallaxObject.transform.lossyScale.x / 2) > depopLimitation.transform.position.x);// probl"me ici
+		}
+		
+	}
+
+
+	float spaceBetweenLastAndPopLimitation() {
+		if (visibleGameObjectTab.Count != 0) {
+			float space = visibleGameObjectTab[visibleGameObjectTab.Count - 1].transform.position.x - popLimitation.transform.position.x;
+			return Mathf.Abs (space);
+
+		} else {
+			return float.MaxValue;
+		}
+	}
+}
---
+++ Assets/parralaxManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ParralaxPlanConfiguration : System.Object
+{
+	public GameObject prefabParralaxPlan;
+	public float distance;
+	public assetGenerator generatorScript;
+	public float lowSpaceBetweenAsset;
+	public float hightSpaceBetweenAsset;
+}
+
+public class parralaxManager : MonoBehaviour {
+	public ParralaxPlanConfiguration[] configurationParralax;
+	public GameObject rightBorder;
+	public GameObject leftBorder;
+	public List<GameObject> parralaxPlans;
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		parralaxPlans.Clear ();
+		foreach (ParralaxPlanConfiguration config in configurationParralax) {
+			GameObject tempParralaxPlan = Instantiate(config.prefabParralaxPlan);
+			tempParralaxPlan.transform.parent = this.transform;
+
+			parallaxPlan tempScript = tempParralaxPlan.GetComponent<parallaxPlan>();
+			tempScript.popLimitation = rightBorder;
+			tempScript.depopLimitation = leftBorder;
+			tempScript.generator = config.generatorScript;
+			tempScript.distance = config.distance;
+			tempScript.lowSpaceBetweenAsset = config.lowSpaceBetweenAsset;
+			tempScript.hightSpaceBetweenAsset = config.hightSpaceBetweenAsset;
+
+			parralaxPlans.Add(tempParralaxPlan);
+		}
+		parralaxPlans.Sort(delegate(GameObject x, GameObject y)
+		{
+			parallaxPlan tempScriptX = x.GetComponent<parallaxPlan>();
+			parallaxPlan tempScriptY = y.GetComponent<parallaxPlan>();
+			if (tempScriptX.distance == tempScriptY.distance) {
+				return 0;
+			} else if (tempScriptX.distance < tempScriptY.distance) {
+				return 1;
+			} else return -1;
+		});
+
+		float zinf = -1.0f;
+		float zsupp = 1.0f;
+		foreach (GameObject temp in parralaxPlans) {
+			if(temp.GetComponent<parallaxPlan>().distance > 0){
+				temp.transform.localPosition = new Vector3(temp.transform.localPosition.x,temp.transform.localPosition.y,temp.transform.localPosition.z + zinf--);
+			} else {
+				temp.transform.localPosition = new Vector3(temp.transform.localPosition.x,temp.transform.localPosition.y,temp.transform.localPosition.z + zsupp++);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject plan in parralaxPlans) {
+			plan.GetComponent<parallaxPlan> ().setSpeedOfPlan (speed);
+		}
+	}
+}
